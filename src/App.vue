130d7ac9e6e0537206a0509f03b52591146a6014
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-03-03 14:41:44 -0300

    base components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,57 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul v-if="fotos.length" class="lista-fotos">
+      <li v-for="(foto, index) in fotos" :key="index" class="lista-fotos-item">
+        <PainelView :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </PainelView>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+  components: {
+    PainelView: () => import("@/components/shared/painel/PainelView.vue"),
+  },
+  data() {
+    return {
+      titulo: "Alura Pick",
+      fotos: [],
+    };
+  },
+  created() {
+    fetch(`http://localhost:3000/v1/fotos`)
+      .then((r) => r.json())
+      .then((r) => {
+        this.fotos = r;
+      });
+  },
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  width: 96%;
+  margin: 0 auto;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos-item {
+  display: inline-block;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+</style>
